c246ae050099019724437c8ba30977eab012678c
OneBit <147380957+one1bit11@users.noreply.github.com>
2025-08-23 19:45:16 +1000

    Added Evil Wizard game manager and also added debug unstuck

--- Godot Project/lumis/Scripts/evil_wizard.gd
+++ Godot Project/lumis/Scripts/evil_wizard.gd
@@ -1,5 +1,6 @@
-extends Node3D
+## This is just the game manager script that will run globally throughout the whole game
 
+extends Node
 
 func _ready() -> void:
 	#traps the mouse in the window mwwahahaahah
@@ -11,3 +12,7 @@ func _input(event: InputEvent) -> void:
 	#frees the mouse, remember to delete this before release this si j
 	if Input.is_action_just_pressed("ExitMenu"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+	## Teleports Mango to a set position
+	if Input.is_key_pressed(KEY_0):
+		$Mango.global_position = Vector3(1.609, 3.242, -5.446)
