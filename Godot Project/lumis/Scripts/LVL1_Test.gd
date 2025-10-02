88f0cf87851ff5181d794a3f3fc9a8883c0d62d7
OneBit <147380957+one1bit11@users.noreply.github.com>
2025-10-03 00:11:34 +1000

    Worked on LV + Added new checkpoints + Fade in/out animation

--- Godot Project/lumis/Scripts/LVL1_Test.gd
+++ Godot Project/lumis/Scripts/LVL1_Test.gd
@@ -2,13 +2,14 @@ extends Node3D
 
 @export var mango:CharacterBody3D
 @export var checkpointPos:Vector3 = Vector3(0,0,0)
+@export var uiAnimation:AnimationPlayer
 
 func reset_mango_to_checkpoint():
 	mango.position = checkpointPos
 
 func _input(event: InputEvent) -> void:
 	if Input.is_key_pressed(KEY_0):
-		reset_mango_to_checkpoint()
+		uiAnimation.play("Respawn")
 
 func _on_checkpoint_area_entered(area: Area3D, extra_arg_0: Vector3) -> void:
 	if area.name == "MangoTrigger":
@@ -17,7 +18,7 @@ func _on_checkpoint_area_entered(area: Area3D, extra_arg_0: Vector3) -> void:
 
 func _on_death_trigger_area_entered(area: Area3D) -> void:
 	if area.name == "MangoTrigger":
-		reset_mango_to_checkpoint()
+		uiAnimation.play("Respawn")
 
 
 func _on_music_replace_area_entered(area: Area3D) -> void:
